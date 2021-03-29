f80b35945780228d629c17f443f173b5b8dd56ff
Matthew Scott <matt@11craft.com>
2021-03-29 14:58:52 -0500

    Adds a more interesting example project

--- src/defaults.ts
+++ src/defaults.ts
@@ -2,10 +2,34 @@ import * as monaco from "monaco-editor"
 
 // language=TypeScript
 export const DEFAULT_SOURCE = `\
-import { Project } from "radiant-voices"
+import { Project, m } from "radiant-voices"
 
 export function createProject(): Project {
-  return new Project()
+  const p = new Project()
+
+  const lfo = m.lfo()
+  p.attach(lfo)
+  lfo.c.generator = true
+  lfo.c.frequencyUnit = m.Lfo.FrequencyUnit.Hz
+  lfo.c.freq = 440
+  lfo.c.waveform = m.Lfo.Waveform.Sin
+
+  const correctDcOffset = m.amplifier()
+  p.attach(correctDcOffset)
+  correctDcOffset.c.dcOffset = -64
+
+  const volume = m.amplifier()
+  p.attach(volume)
+  volume.c.fineVolume = 12345
+
+  const vibrato = m.vibrato()
+  p.attach(vibrato)
+
+  lfo.linkTo(correctDcOffset).linkTo(vibrato).linkTo(volume)
+
+  volume.linkTo(p.outputModule)
+
+  return p
 }
 `
 
